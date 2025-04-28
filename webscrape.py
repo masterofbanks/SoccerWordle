bb342445aa491fd4d89e527c0a2e0b6b04292992
masterofbanks <tarikalex2003@gmail.com>
2025-04-28 16:57:59 -0400

    navigating club websites

--- webscrape.py
+++ webscrape.py
@@ -19,22 +19,48 @@ clubs = subMenu.find_all('tr')
 liverpool = clubs
 x = 0
 
-listOfTitles = []
-for club in clubs:
-    time.sleep(10)
-    td = club.find('td')
-    a = td.find('a')
-    newEnding = a.get('href')
-    withoutSlash = newEnding[1:]
-    new_url = top_url + withoutSlash
-    newRequest = requests.get(new_url)
-    print(newRequest.ok)
-    newSoup = BeautifulSoup(newRequest.content, 'html.parser')
-    top = newSoup.find('div', id = 'info')
-    subMenu = top.find('h1')
-    title = subMenu.find('span')
-    title_string = title.getText()
-    listOfTitles.append(title_string)
+time.sleep(5)
+td = clubs[0].find('td')
+a = td.find('a')
+newEnding = a.get('href')
+withoutSlash = newEnding[1:]
+new_url = top_url + withoutSlash
+newRequest = requests.get(new_url)
+print(newRequest.ok)
+newSoup = BeautifulSoup(newRequest.content, 'html.parser')
+top = newSoup.find('div', id = 'info')
+subMenu = top.find('h1')
+title = subMenu.find('span')
+title_string = title.getText()
+print(title_string)
+
+tableOfPlayers = newSoup.find('div', id = 'all_stats_standard')
+subMenu = newSoup.find('tbody')
+players = subMenu.find_all('tr')
+playerNames = []
+for player in players:
+    name = player.find('th').get_text()
+    print(name)
+    playerNames.append(name)
+print(len(playerNames))
+
+
+#listOfTitles = []
+# for club in clubs:
+#     time.sleep(10)
+#     td = club.find('td')
+#     a = td.find('a')
+#     newEnding = a.get('href')
+#     withoutSlash = newEnding[1:]
+#     new_url = top_url + withoutSlash
+#     newRequest = requests.get(new_url)
+#     print(newRequest.ok)
+#     newSoup = BeautifulSoup(newRequest.content, 'html.parser')
+#     top = newSoup.find('div', id = 'info')
+#     subMenu = top.find('h1')
+#     title = subMenu.find('span')
+#     title_string = title.getText()
+#     listOfTitles.append(title_string)
     
 
-print(listOfTitles)
+#print(listOfTitles)
